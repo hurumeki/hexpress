0f636d9f6cadbe09c580f2120e7444be679ebbfb
hurumeki <hurumeki1987@gmail.com>
2026-03-09 12:35:17 +0900

    chore: remove explicit editor UI buttons while keeping URL-based access

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import StageEditor from './editor/StageEditor.tsx'
+import App from './App'
+import StageEditor from './editor/StageEditor'
 
-const isEditor = window.location.pathname === '/edit';
+const isEditor = window.location.pathname.endsWith('/edit') ||
+    window.location.search.includes('edit=1') ||
+    window.location.hash === '#edit';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
