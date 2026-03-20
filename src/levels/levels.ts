25b06f8f32f8208e018e2e7b3641991bc40e9d38
hurumeki <hurumeki1987@gmail.com>
2026-03-20 10:47:12 +0900

    rename and sort levels

--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -3,17 +3,13 @@ import Tutorial_1 from './Tutorial_1.json';
 import Tutorial_2 from './Tutorial_2.json';
 import Tutorial_3 from './Tutorial_3.json';
 import Rotate_Triangle from './Rotate_Triangle.json';
-import Stage_5 from './Stage_5.json';
 import Stage_6 from './Stage_6.json';
 import Stage_7 from './Stage_7.json';
-import Stage_8 from './Stage_8.json';
 import Stage_9 from './Stage_9.json';
 import Stage_10 from './Stage_10.json';
-import Stage_11 from './Stage_11.json';
 import Stage_12 from './Stage_12.json';
-import Stage_13 from './Stage_13.json';
 import Rotate_Triangle_2 from './Rotate_Triangle_2.json';
-import passing_each_other from './passing_each_other.json';
+import Passing from './Passing.json';
 import Ring from './Ring.json';
 import Windmill from './Windmill.json';
 import Frog from './Frog.json';
@@ -23,14 +19,14 @@ import Bone from './Bone.json';
 import Diamond from './Diamond.json';
 import Diamond_2 from './Diamond_2.json';
 import Diamond_3 from './Diamond_3.json';
-import Save_a_move from './Save_a_move.json';
+import Wall from './Wall.json';
 import X from './X.json';
 import Mouse from './Mouse.json';
 import Triangle_27 from './Triangle_27.json';
 import Simply from './Simply.json';
-import Practice_2 from './Practice_2.json';
+import A from './A.json';
 import Hot_Dog from './Hot_Dog.json';
-import three_way from './three-way.json';
+import Three_way from './Three-way.json';
 import Clover from './Clover.json';
 import Training_3 from './Training_3.json';
 import Triangle_34 from './Triangle_34.json';
@@ -41,38 +37,36 @@ export const LEVELS: Level[] = [
     Tutorial_1 as Level,
     Tutorial_2 as Level,
     Tutorial_3 as Level,
-    Rotate_Triangle as Level,
-    Stage_5 as Level,
-    Stage_6 as Level,
-    Stage_7 as Level,
-    Stage_8 as Level,
-    Stage_9 as Level,
-    Stage_10 as Level,
-    Stage_11 as Level,
-    Stage_12 as Level,
-    Stage_13 as Level,
-    Rotate_Triangle_2 as Level,
-    passing_each_other as Level,
-    Ring as Level,
-    Windmill as Level,
-    Frog as Level,
-    M as Level,
-    Practice as Level,
-    Bone as Level,
-    Diamond as Level,
-    Diamond_2 as Level,
-    Diamond_3 as Level,
-    Save_a_move as Level,
-    X as Level,
-    Mouse as Level,
-    Triangle_27 as Level,
-    Simply as Level,
-    Practice_2 as Level,
-    Hot_Dog as Level,
-    three_way as Level,
-    Clover as Level,
-    Training_3 as Level,
-    Triangle_34 as Level,
     Training_1 as Level,
     Training_2 as Level,
+    Training_3 as Level,
+    Rotate_Triangle as Level,
+    Practice as Level,
+    Passing as Level,
+    A as Level,
+    Rotate_Triangle_2 as Level,
+    Triangle_34 as Level,
+
+    Bone as Level,
+    Frog as Level,
+    Stage_6 as Level,
+    Diamond as Level,
+    Stage_7 as Level,
+    Mouse as Level,
+    Windmill as Level,
+    Diamond_2 as Level,
+    Stage_9 as Level,
+    Stage_10 as Level,
+    X as Level,
+    Diamond_3 as Level,
+
+    Hot_Dog as Level,
+    Stage_12 as Level,
+    Simply as Level,
+    M as Level,
+    Ring as Level,
+    Wall as Level,
+    Triangle_27 as Level,
+    Three_way as Level,
+    Clover as Level,
 ];
