33c9eae5070e5b44030bfd7147b87cb3d43b14a4
hurumeki <hurumeki1987@gmail.com>
2026-03-20 12:15:49 +0900

    add levels

--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -1,37 +1,40 @@
 import type { Level } from '../types';
-import Tutorial_1 from './Tutorial_1.json';
-import Tutorial_2 from './Tutorial_2.json';
-import Tutorial_3 from './Tutorial_3.json';
-import Rotate_Triangle from './Rotate_Triangle.json';
-import Stage_6 from './Stage_6.json';
-import Stage_7 from './Stage_7.json';
-import Stage_9 from './Stage_9.json';
-import Stage_10 from './Stage_10.json';
-import Stage_12 from './Stage_12.json';
-import Rotate_Triangle_2 from './Rotate_Triangle_2.json';
-import Passing from './Passing.json';
-import Ring from './Ring.json';
-import Windmill from './Windmill.json';
-import Frog from './Frog.json';
-import M from './M.json';
-import Practice from './Practice.json';
+import A from './A.json';
 import Bone from './Bone.json';
+import Clover from './Clover.json';
+import Crystal from './Crystal.json';
+import Crystal_2 from './Crystal_2.json';
 import Diamond from './Diamond.json';
 import Diamond_2 from './Diamond_2.json';
 import Diamond_3 from './Diamond_3.json';
-import Wall from './Wall.json';
-import X from './X.json';
-import Mouse from './Mouse.json';
-import Triangle_27 from './Triangle_27.json';
-import Simply from './Simply.json';
-import A from './A.json';
+import Donut from './Donut.json';
+import Frog from './Frog.json';
 import Hot_Dog from './Hot_Dog.json';
+import M from './M.json';
+import Mouse from './Mouse.json';
+import Passing from './Passing.json';
+import Practice from './Practice.json';
+import Ring from './Ring.json';
+import Rotate_Triangle from './Rotate_Triangle.json';
+import Rotate_Triangle_2 from './Rotate_Triangle_2.json';
+import Simply from './Simply.json';
+import Stage_10 from './Stage_10.json';
+import Stage_12 from './Stage_12.json';
+import Stage_6 from './Stage_6.json';
+import Stage_7 from './Stage_7.json';
+import Stage_9 from './Stage_9.json';
 import Three_way from './Three-way.json';
-import Clover from './Clover.json';
-import Training_3 from './Training_3.json';
-import Triangle_34 from './Triangle_34.json';
 import Training_1 from './Training_1.json';
 import Training_2 from './Training_2.json';
+import Training_3 from './Training_3.json';
+import Triangle_27 from './Triangle_27.json';
+import Triangle_34 from './Triangle_34.json';
+import Tutorial_1 from './Tutorial_1.json';
+import Tutorial_2 from './Tutorial_2.json';
+import Tutorial_3 from './Tutorial_3.json';
+import Wall from './Wall.json';
+import Windmill from './Windmill.json';
+import X from './X.json';
 
 export const LEVELS: Level[] = [
     Tutorial_1 as Level,
@@ -49,19 +52,22 @@ export const LEVELS: Level[] = [
 
     Bone as Level,
     Frog as Level,
+    Crystal as Level,
     Stage_6 as Level,
     Diamond as Level,
+    Donut as Level,
     Stage_7 as Level,
-    Mouse as Level,
-    Windmill as Level,
     Diamond_2 as Level,
     Stage_9 as Level,
-    Stage_10 as Level,
-    X as Level,
+    Mouse as Level,
     Diamond_3 as Level,
+    Windmill as Level,
+    X as Level,
 
-    Hot_Dog as Level,
+    Stage_10 as Level,
     Stage_12 as Level,
+    Hot_Dog as Level,
+    Crystal_2 as Level,
     Simply as Level,
     M as Level,
     Ring as Level,
